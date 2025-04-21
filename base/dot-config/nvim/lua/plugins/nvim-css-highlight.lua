11fa5df5e4fb96cc1f4d68c3abfc1d739b2d6a6f
Toby Dickenson <tbdickenson@googlemail.com>
2025-04-21 22:29:29 +0100

    gutting and reorganising nvim config ready for a rewrire tomorrow :)

---
+++ base/dot-config/nvim/lua/plugins/nvim-css-highlight.lua
@@ -0,0 +1,12 @@
+return {
+    {"brenoprata10/nvim-highlight-colors", -- nice highlighting for css colours
+        opts = {
+            render = 'background',
+            --render = 'foreground',
+            --render = 'virtual',
+            virtual_symbol_position = 'eow',--'eol', 'inline'
+            virtual_symbol_prefix = '',
+            virtual_symbol_suffix = ' ',
+        }
+    },
+}
--- base/dot-config/nvim/lua/plugins/treesitter.lua
+++ base/dot-config/nvim/lua/plugins/treesitter.lua
@@ -1,4 +1,14 @@
 return {
+
+
+    {"jiaoshijie/undotree",
+        dependencies = "nvim-lua/plenary.nvim",
+        config = true,
+        keys = { -- load the plugin only when using it's keybinding:
+            { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
+        },
+    },
+
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
