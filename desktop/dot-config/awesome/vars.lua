40127e4591eabb9114e37343302fbd3173ef2b2b
Toby Dickenson <tbdickenson@googlemail.com>
2025-09-26 17:32:52 +0100

    small update

--- desktop/dot-config/awesome/vars.lua
+++ desktop/dot-config/awesome/vars.lua
@@ -11,11 +11,10 @@ return {
     editor = os.getenv("EDITOR") or "nvim",
     wallpaper = "feh --bg-fill $(ls -d $HOME/Wallpaper/* | shuf | head -1)",
     tags = {
-        { "main", "a", awful.layout.layouts[1] },
-        { "web", "s", awful.layout.layouts[1] },
-        { "notes", "d", awful.layout.layouts[2] },
-        { "chat", "f", awful.layout.layouts[3] },
-        { "media", "g", awful.layout.layouts[3] },
+        { "main",  "a", awful.layout.layouts[1] },
+        { "web",   "s", awful.layout.layouts[1] },
+        { "term",  "d", awful.layout.layouts[2] },
+        { "notes", "f", awful.layout.layouts[3] },
+        { "misc",  "g", awful.layout.layouts[3] },
     },
 }
-
