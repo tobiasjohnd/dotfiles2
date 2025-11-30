94774d365f3e4d00672a638a1c288b17a5ab4a51
Toby Dickenson <tbdickenson@googlemail.com>
2025-11-30 16:33:16 +0000

    add iron.nvim to config

---
+++ base/dot-config/nvim/lua/plugins/iron.lua
@@ -0,0 +1,31 @@
+return {
+    'Vigemus/iron.nvim',
+    config = function()
+        local iron = require('iron.core')
+        iron.setup({
+            config = {
+                repl_definition = {
+                    python = {
+                        command = { 'ipython' },
+                        block_dividers = { '# %%', '#%%' },
+                        format = require('iron.fts.common').bracketed_paste
+                    }
+                },
+                repl_open_cmd = require('iron.view').split.vertical.rightbelow(0.4),
+            },
+            keymaps = {
+                send_motion = '\\',
+                visual_send = '\\',
+                send_line = '\\\\',
+                send_file = '<leader>if',
+                send_code_block = '<leader>ib',
+                send_code_block_and_move = '<leader>in',
+                send_until_cursor = "<space>iu",
+                cr = '<leader>i<cr>',
+                interrupt = '<leader>i<space>',
+                exit = '<leader>iq',
+                clear = '<leader>ic',
+            },
+        })
+    end
+}
