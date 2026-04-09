497a224ff899ae3956e748fd99aa70b3b9bd7f0b
Toby Dickenson <tbdickenson@googlemail.com>
2026-04-09 21:18:04 +0100

    add clangd LSP support

--- neovim/dot-config/nvim/lua/lsp.lua
+++ neovim/dot-config/nvim/lua/lsp.lua
@@ -47,5 +47,5 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end
 })
 
-vim.lsp.enable({ 'luals', 'pyright', 'gopls' })
+vim.lsp.enable({ 'luals', 'pyright', 'gopls', 'clangd' })
 -- jdtls not enabled here nvim-jdtls sorts itself out.
